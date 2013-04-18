85191aae72fd60e00fbf5f8f2b006a1122a88764
Paul Tannenbaum <paultannenbaum@gmail.com>
2013-04-18 13:06:41 -0700

    commiting deleted files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Baumer::Application.config.secret_token = 'b9218cc9af3ac72995396f140e1ec1953bd6ef03fcdb0c412f7e9be835d90db0a9cca9afccb78f8ed1103c8a59ff0f352d563afcf1cf74349b566037ef4c153c'
